78d486cdaac5458f37963ef018464ae808ce9cde
cjjimenez1 <58470655+cjjimenez1@users.noreply.github.com>
2019-12-05 13:40:12 +0100

    Update suma.php

--- suma.php
+++ suma.php
@@ -29,3 +29,17 @@
         ?>
     </body>
 </html>
+
+//Ejemplo de una función que devuelve un array
+<?php
+   function sumar_restar($num1, $num2){
+      $resultado = array(
+         'suma' => $num1 + $num2,
+         'resta' => $num1 - $num2
+      );
+      return $resultado;
+   }
+   $resultado_de_suma_resta = sumar_restar(20, 7);
+   echo 'La suma es: ' . $resultado_de_suma_resta['suma'] . '<br />';
+   echo 'La resta es: ' . $resultado_de_suma_resta['resta'] . '<br />';
+?>
